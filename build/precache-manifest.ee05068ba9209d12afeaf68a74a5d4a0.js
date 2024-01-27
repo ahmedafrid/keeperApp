e93e3b1158617a0ce9f258b7fde878f02836c7c5
ahmedafrid <ahmedafridofficial@gmail.com>
2024-01-27 12:03:19 +0530

    build folder

---
+++ build/precache-manifest.ee05068ba9209d12afeaf68a74a5d4a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fa5ea521cae4d2394bf437ba94d9a5",
+    "url": "%7Bgithub-username%7D.github.io/%7Brepo-name%7D/index.html"
+  },
+  {
+    "revision": "8783906c092f29e2c279",
+    "url": "%7Bgithub-username%7D.github.io/%7Brepo-name%7D/static/js/2.9fee9c4c.chunk.js"
+  },
+  {
+    "revision": "1677dbc0174de6ac23ae",
+    "url": "%7Bgithub-username%7D.github.io/%7Brepo-name%7D/static/js/main.28853692.chunk.js"
+  },
+  {
+    "revision": "ef0ddd1d569cef1998d1",
+    "url": "%7Bgithub-username%7D.github.io/%7Brepo-name%7D/static/js/runtime-main.fc52b88c.js"
+  }
+]);
